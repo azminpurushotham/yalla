cf12ab5cda585b91befc946e99deae3c5732f9f0
asminpurushotham <asminpurushotham@gmail.com>
2022-01-02 00:21:55 +0530

    joblist completed

--- app/src/main/java/com/cherry/yalla/JobListFragment.kt
+++ app/src/main/java/com/cherry/yalla/JobListFragment.kt
@@ -1,17 +1,25 @@
 package com.cherry.yalla
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.log
 
 class JobListFragment : Fragment() {
 
+    private val TAG: String = "JobListFragment"
     val months: ArrayList<String> = ArrayList()
-    lateinit var rvMonths: RecyclerView;
+    val dates: ArrayList<String> = ArrayList()
+    lateinit var rvMonths: RecyclerView
+    lateinit var rvDate: RecyclerView
+    lateinit var rvJobs: RecyclerView
     lateinit var adapter: MonthAdapter
+    lateinit var datesAdapter: DatesAdapter
+    lateinit var jobsAdapter: JobsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +35,34 @@ class JobListFragment : Fragment() {
         months.add("Oct")
         months.add("Nov")
         months.add("Dec")
+
+        for (i in 1..30) {
+            dates.add(i.toString())
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        print(" onCreateView ")
+        Log.v("onCreateView", TAG)
+        val view = inflater.inflate(R.layout.fragment_joblist, container, false)
         rvMonths = view.findViewById(R.id.rvMonths)
+        rvDate = view.findViewById(R.id.rvDate)
+        rvJobs = view.findViewById(R.id.rvJobs)
+
         adapter = MonthAdapter(months)
+        rvMonths.adapter = adapter
+
+        print(" Size -->> ${dates.size}")
+        Log.v(" Size -->> ${dates.size}", TAG)
+        datesAdapter = DatesAdapter(dates)
+        rvDate.adapter = datesAdapter
+
+        jobsAdapter = JobsAdapter(months)
+        rvJobs.adapter = jobsAdapter
+
         return view
     }
 }
--- app/src/main/java/com/cherry/yalla/MainActivity.kt
+++ app/src/main/java/com/cherry/yalla/MainActivity.kt
@@ -12,21 +12,29 @@ class MainActivity : FragmentActivity() {
 
     lateinit var laySettings: LinearLayout
     lateinit var layProfile: LinearLayout
+    lateinit var layJobs: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         laySettings = findViewById(R.id.laySettings)
         layProfile = findViewById(R.id.layProfile)
+        layJobs = findViewById(R.id.layJobs)
 
         laySettings.setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
         }
+
         layProfile.setOnClickListener {
             findNavController(this,R.id.container).navigate(R.id.profileFragment)
         }
+
         layProfile.setOnClickListener {
             findNavController(this,R.id.container).navigate(R.id.profileFragment)
+        }
+
+        layJobs.setOnClickListener {
+            findNavController(this,R.id.container).navigate(R.id.jobListFragment)
         }
     }
 }
