1d9f874dad6d14c1eb35e397039fc30c48fdb66b
Nijas M <nijas@parelstudio.com>
2022-01-02 21:56:31 +0530

    design fixes, navigation

---
+++ app/src/main/java/com/cherry/yalla/screens/order/MyJobActivity.kt
@@ -0,0 +1,56 @@
+package com.cherry.yalla.screens.order
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.application.base.BaseActivity
+import com.cherry.yalla.databinding.ActivityJobBinding
+import com.cherry.yalla.databinding.ActivityLogBinding
+import com.cherry.yalla.databinding.ActivityMyJobBinding
+import com.cherry.yalla.model.JobModel
+import com.cherry.yalla.screens.job.JobActivity
+import com.cherry.yalla.screens.job.JobDetailActivity
+import com.cherry.yalla.screens.job.adapter.JobAdapter
+import com.cherry.yalla.screens.order.adapter.ActivityAdapter
+import com.cherry.yalla.screens.order.adapter.JobListAdapter
+
+class MyJobActivity : BaseActivity(), BaseActivity.OnRetryButtonClick {
+
+    private lateinit var binding: ActivityMyJobBinding
+    var jobList = arrayListOf<JobModel>()
+    lateinit var jobAdapter: JobListAdapter
+
+    override fun getContentView(): View {
+        binding = ActivityMyJobBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
+
+    override fun getRetryListener(): OnRetryButtonClick {
+        return this
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        binding.header.tvHeading.text="My Job"
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerJob.setHasFixedSize((false))
+        jobAdapter= JobListAdapter(this,jobList, object : JobListAdapter.OnItemClicked {
+            override fun onRecyclerItemClicked(data: JobModel) {
+                startActivity(Intent(this@MyJobActivity, JobActivity::class.java))
+            }
+
+        })
+        binding.recyclerJob.adapter=jobAdapter
+    }
+
+    override fun onRetryClick() {
+
+    }
+}
---
+++ app/src/main/java/com/cherry/yalla/screens/order/adapter/NotificationAdapter.kt
@@ -0,0 +1,58 @@
+package com.cherry.yalla.screens.order.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.cherry.yalla.R
+import com.cherry.yalla.model.JobModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+class NotificationAdapter(
+    private val mContext: Context,
+    private val mApps: ArrayList<JobModel>,
+    var listener: OnItemClicked
+) : RecyclerView.Adapter<NotificationAdapter.ViewHolder>() {
+
+    lateinit var onItemClicked: OnItemClicked
+
+    interface OnItemClicked {
+        fun onRecyclerItemClicked(data: JobModel)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        onItemClicked = listener
+        return ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_notification_job, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvAccept.setOnClickListener {
+            listener.onRecyclerItemClicked(JobModel())
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        var tvAccept: RelativeLayout
+        var tvReject: RelativeLayout
+
+        init {
+            tvAccept = itemView.findViewById<View>(R.id.tvAccept) as RelativeLayout
+            tvReject = itemView.findViewById<View>(R.id.tvReject) as RelativeLayout
+        }
+    }
+}
