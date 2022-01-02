557a3a8b3e42ae93e980e513de93be1ba7964376
asminpurushotham <asminpurushotham@gmail.com>
2022-01-02 13:54:32 +0530

    bottom panel completed

--- app/src/main/java/com/cherry/yalla/SettingsActivity.kt
+++ app/src/main/java/com/cherry/yalla/SettingsActivity.kt
@@ -1,15 +1,20 @@
 package com.cherry.yalla
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 
 class SettingsActivity : ComponentActivity() {
-    lateinit var btnLogin: Button
+    lateinit var btnBack: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+        btnBack = findViewById(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            finish()
+        }
+
     }
 }
