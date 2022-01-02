03d4c13aab6e11e63b918473f24068dbff16bce3
asminpurushotham <asminpurushotham@gmail.com>
2022-01-02 20:57:33 +0530

    working on issues

--- app/src/main/java/com/cherry/yalla/SplashActivity.kt
+++ app/src/main/java/com/cherry/yalla/SplashActivity.kt
@@ -5,14 +5,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.annotation.MainThread
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.cherry.yalla.ui.theme.YallaTheme
 
 class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
